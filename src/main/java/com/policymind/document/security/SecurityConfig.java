942b30c7d62927e4d47a24e253e794219a7b09bb
Ravi Kafley <rkafley.dev@gmail.com>
2026-03-16 18:39:31 -0400

    Fix security diagnostics build import

--- src/main/java/com/policymind/document/security/SecurityConfig.java
+++ src/main/java/com/policymind/document/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.policymind.document.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
